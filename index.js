31fcb3dcaee60c085a86cba74210331fd1153962
Ben L <veloswim@gmail.com>
2023-02-01 08:48:07 -0800

    added utils folder and declared several functions

--- index.js
+++ index.js
@@ -22,25 +22,73 @@ const init = async () => {
         "Add a role",
         "Add an employee",
         "Update an employee role",
+        "Quit",
       ],
       name: "mainMenuChoice",
     },
   ]);
+  const { mainMenuChoice } = mainMenu;
 
   switch (mainMenuChoice) {
     case "View all departments":
+      viewDepartments();
       break;
     case "View all roles":
+      viewEmployeeRoles();
       break;
     case "View all employees":
+      viewEmployees();
       break;
     case "Add a department":
+      addNewDepartment();
       break;
     case "Add a role":
+      addRole();
       break;
     case "Add an employee":
+      addNewEmployee();
       break;
     case "Update an employee role":
+      updateEmployee();
       break;
+    case "Quit":
+      console.log("Goodbye!");
   }
 };
+
+const viewDepartments = async () => {
+  console.log("You chose viewDeps");
+  init();
+};
+
+const viewEmployeeRoles = async () => {
+  console.log("You chose view empRoles");
+  init();
+};
+
+const viewEmployees = async () => {
+  console.log("You chose viewEmps");
+  init();
+};
+
+const addNewDepartment = async () => {
+  console.log("You chose addDep");
+  init();
+};
+
+const addRole = async () => {
+  console.log("You chose addRole");
+  init();
+};
+
+const addNewEmployee = async () => {
+  console.log("You chose addNewEmp");
+  init();
+};
+
+const updateEmployee = async () => {
+  console.log("You chose updEmp");
+  init();
+};
+
+init();
