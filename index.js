77f183877aadecef5fb2fd5dd3c7d44bf4993482
Ben L <veloswim@gmail.com>
2023-02-01 08:31:42 -0800

    added main menu inquirer prompt

--- index.js
+++ index.js
@@ -8,3 +8,39 @@ const db = mysql.createConnection({
   database: "employees_db",
   multipleStatements: true,
 });
+
+const init = async () => {
+  const mainMenu = await inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+      ],
+      name: "mainMenuChoice",
+    },
+  ]);
+
+  switch (mainMenuChoice) {
+    case "View all departments":
+      break;
+    case "View all roles":
+      break;
+    case "View all employees":
+      break;
+    case "Add a department":
+      break;
+    case "Add a role":
+      break;
+    case "Add an employee":
+      break;
+    case "Update an employee role":
+      break;
+  }
+};
